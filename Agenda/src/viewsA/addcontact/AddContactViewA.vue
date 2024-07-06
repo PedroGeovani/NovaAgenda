4595695b29b31f9a8a0bbbf3fbfcdf521fa3e4df
Pedro Geovani <pedro.geovanisc@gmail.com>
2024-07-06 20:39:51 -0300

    finaly show details

--- Agenda/src/viewsA/addcontact/AddContactViewA.vue
+++ Agenda/src/viewsA/addcontact/AddContactViewA.vue
@@ -1,9 +1,17 @@
 <template>
   <div>
-    Adicionar A
-
+    Adicionar Novo Contato
+    <div> 
+      <p> Nome: <input>  </input> </p> 
+      <p> Endereço: <input>  </input></p>
+      <p> Cidade: <input>  </input></p>
+      <p> Telefone: <input>  </input></p>
+      <p> Email: <input>  </input></p>
+      <button> Salvar </button> <button> Descartar </button>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
+
 </script>
--- Agenda/src/viewsA/contact/ContactViewA.vue
+++ Agenda/src/viewsA/contact/ContactViewA.vue
@@ -1,28 +1,54 @@
 <template>
-    <div v-for="(user,index) in users" :key="index"> Nome: {{ user.name }}
-        <router-link :to="{path:'/DetailsContactA/'+user.id, params:user.id}"> Detalhes </router-link>
-    </div>
+    <div v-for="(user,index) in users" :key="index" > Nome: {{ user.name }}  
+        <button @click="i=index" v-if="i === -1"> Mostrar Detalhes </button> 
+        <div v-if="i === index"> 
+        <DetailsViewA 
+        :address="user.address" 
+        :city="user.city" 
+        :phone="user.phone" 
+        :email="user.email"/> 
+        <button @click="i = -1"> Esconder Detalhes </button>
+        <button @click="exclude(String(user.id)), i=-1"> Excluir contato </button> 
+        </div>
+    </div>  
     </template>
     
     <script lang="ts">
     import { api } from '@/api/api';
     import { UserTypes } from '@/api/typesUser'
+    import DetailsViewA from '../details/DetailsViewA.vue';
+
     export default {
+        components:{
+            DetailsViewA
+        },
         data(){
             return{
                 users: new Array<UserTypes>(),
+                i : -1
             }
-        }, 
+        },         
         mounted(){ 
             api
             .get("/users")
             .then(response => (this.users = response.data)) 
             .catch(err =>  console.log(err));            
+        },
+        methods:{
+            exclude: function(id: string){
+            api
+            .delete("/users/"+id)
+            .then(() => {
+                    api
+                    .get("/users")
+                    .then(response => (this.users = response.data)) 
+                    .catch(err =>  console.log(err))}
+            )},
         }
     }
-    </script>
+</script>
 
-    <style>
-   
-    </style>
+<style>
+
+</style>
 
